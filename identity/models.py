2773f51dce1e72362c34cc7d420c3ee5aa221354
JRParreno <jhonrhayparreno22@gmail.com>
2022-02-13 10:56:14 +0800

    initial register

--- identity/models.py
+++ identity/models.py
@@ -6,16 +6,20 @@ class Identity(models.Model):
 
     MALE = 'M'
     FEMALE = 'F'
+    NA = 'N/A'
 
     GENDER_CHOICES = [
         (MALE, 'Male'),
         (FEMALE, 'Female'),
+        (NA, 'N/A'),
     ]
 
     birthdate = models.DateField(null=True)
     profile_picture = models.ImageField(upload_to='profile-pictures/', blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default=MALE)
+    gender = models.CharField(max_length=5, choices=GENDER_CHOICES, default=NA)
 
     def __str__(self):
         return self.user.last_name
+
+
--- identity/views.py
+++ identity/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions, response, status
+from .models import Identity
+from .serializers import RegisterSerializer, ProfileSerializer
+from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
 
-# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    permission_classes = []
+    queryset = Identity.objects.all()
+    serializer_class = RegisterSerializer
+
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+    serializer_class = ProfileSerializer
+
+    def get_object(self):
+        user = self.request.user
+        if Identity.objects.filter(user=user).exists():
+            return user.identity
+        else:
+            user.identity = Identity(user=user)
+            user.save()
+            return user.identity
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context.update({
+            'request': self.request
+        })
+        return context
+
+
